931b8e0a8ed862651826cdd916f45dc874db7219
Sandro <sandroqarqusashvili@gmail.com>
2025-10-17 23:44:48 -0400

    changed structure

---
+++ memora/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h1 className="title">Memora</h1>
+      <p className="subtitle">Frontend connection ready</p>
+    </div>
+  );
+}
